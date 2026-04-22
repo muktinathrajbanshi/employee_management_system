b9ec9786dd98f9ff6aeef3b43c5019ae434c2fe1
unknown <muktinathrajbanshi786@gmail.com>
2026-04-22 22:24:22 +0545

    k gardai chhas yar muktinath yasari kaha hunchha ani

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Toaster } from "react-hot-toast"
 import { Route, Routes } from "react-router-dom"
 import LoginLanding from "./pages/LoginLanding"
+import Layout from "./pages/Layout"
 
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
       <Toaster />
       <Routes>
         <Route path="/login" element={<LoginLanding />}/>
+        <Route element={<Layout />}/>
+
       </Routes>
     </>
   )
