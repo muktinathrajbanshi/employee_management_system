d4a5c54304e1ee0ded36f2a3fe65016ed3d32302
unknown <muktinathrajbanshi786@gmail.com>
2026-04-22 22:29:07 +0545

    hait yar k saro fase ma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import LoginLanding from "./pages/LoginLanding"
 import Layout from "./pages/Layout"
 import Dashboard from "./pages/Dashboard"
+import Employees from "./pages/Employees"
 
 
 const App = () => {
@@ -13,6 +14,13 @@ const App = () => {
         <Route path="/login" element={<LoginLanding />}/>
         <Route element={<Layout />}/>
          <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/employees" element={<Employees />} />
+         <Route path="/attendance" element={<Dashboard />} />
+         <Route path="/leave" element={<Dashboard />} />
+         <Route path="/payslips" element={<Dashboard />} />
+         <Route path="/settings" element={<Dashboard />} />
+
+
       </Routes>
     </>
   )
