35420f8465331a2b14fc04c58f3f468651559840
unknown <muktinathrajbanshi786@gmail.com>
2026-04-22 23:06:23 +0545

    la mukti yar voli dekhi chi alik badi code garyati le 2 barsa ko yojana 4 barsa lagchha feri ni

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,8 +1,15 @@
+import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div>
-      Layout
+    <div className="flex h-screen bg-linear-to-br from-state-50
+    via-white to-indigo-50/30">
+      <p>Sidebar</p>
+      <main>
+        <div>
+          <Outlet />
+        </div>
+      </main>
     </div>
   )
 }
