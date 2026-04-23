44d78944863a300f5c6afc08d1f843a499c94c98
unknown <muktinathrajbanshi786@gmail.com>
2026-04-23 20:10:21 +0545

    vok lago my lord haha

--- frontend/src/pages/LoginLanding.jsx
+++ frontend/src/pages/LoginLanding.jsx
@@ -4,6 +4,26 @@ const LoginLanding = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <LoginLeftSide />
+
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-center
+      p-6 sm:p-12 lg:p-16 relative overflow-y-auto min-h-screen">
+
+      <div className="w-full max-w-md animate-fade-in relative z-10">
+
+        {/* Header  */}
+        <div>
+          <h2>Welcome Back</h2>
+          <p>Select your portal to securely access the system.</p>
+        </div>
+
+        {/* Portal List  */}
+
+        { /* Footer */ }
+
+      </div>
+
+      </div>
+
     </div>
   )
 }
