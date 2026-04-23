38bd36c62d1a31fdfc97821fa8a9872598e61c2e
unknown <muktinathrajbanshi786@gmail.com>
2026-04-23 19:58:26 +0545

    yo para le kaise saknu yo project vanata haha

---
+++ frontend/src/components/LoginLeftSide.jsx
@@ -0,0 +1,20 @@
+
+const LoginLeftSide = () => {
+  return (
+    <div className="hidden md:flex w-1/2 bg-indigo-950
+    relative overflow-hidden border-r border-slate-200">
+      
+    <div className="absolute -top-30 -left-30 w-72
+    h-72 bg-indigo-500/20 rounded-full blur-3xl"></div>
+
+    <div>
+    <h1>Employee <br /> Management System</h1>
+    <p>Streamline your workforce operations, track attendance, manage payroll, and 
+    empower your team securely</p>
+    </div>
+
+    </div>
+  )
+}
+
+export default LoginLeftSide
