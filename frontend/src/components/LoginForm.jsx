89bb7b4dd821d98d4b837e8de87f4deb1b4c36b4
unknown <muktinathrajbanshi786@gmail.com>
2026-04-24 09:44:09 +0545

    focus muktinath focus haha

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,15 +2,54 @@ import { Link } from "react-router-dom"
 import LoginLeftSide from "./LoginLeftSide"
 import { ArrowLeftIcon } from "lucide-react"
 
-const LoginForm = () => {
+const LoginForm = ({role, title, subtitle}) => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
+
       <LoginLeftSide />
-      <div className="w-full max-w-md animate-fade-in">
-        <Link to="/login" className="inline-flex items-center gap-2 text-slate-400 hover:text-slate-700
-        text-sm mb-10 transition-colors">
-            <ArrowLeftIcon size={16} /> Back to portals
-        </Link>
+
+      <div className="flex-1 flex items-center justify-center p-6 sm:p-12 bg-white">
+         <div className="w-full max-w-md animate-fade-in">
+            <Link to="/login" className="inline-flex items-center gap-2 text-slate-400 hover:text-slate-700
+            text-sm mb-10 transition-colors">
+                <ArrowLeftIcon size={16} /> Back to portals
+            </Link>
+
+            <div className="mb-8">
+              <h1 className="text-2xl sm:text-3xl font-medium text-zinc-800">{title}</h1>
+              <p className="text-slate-500 text-sm sm:text-base mt-2">{subtitle}</p>
+            </div>
+
+            {error && (
+              <div className="mb-6 p-4 bg-rose-50 border border-rose-200 text-rose-700
+              text-sm rounded-xl flex items-start gap-3">
+              <div className="w-1.5 h-1.5 rounded-full bg-rose-500 mt-1.5 shrink-0" />
+                {error}
+              </div>
+            )}
+
+            <from className="space-y-5" onSubmit={handleSubmit}>
+              <div>
+                <label className="block text-sm font-medium text-slate-700 mb-2">Email address</label>
+                <input type="email" value={email} onChange={(e) => {
+                  setEmail(e.target.value)
+                }} />
+              </div>
+            </from>
+
+         </div>
       </div>
     </div>
   )
