93960b837abb9c1fd702d6c45705a8c93bd37a5f
unknown <muktinathrajbanshi786@gmail.com>
2026-04-24 23:09:14 +0545

    aja lai yati yar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import { dummyProfileData } from "../assets/assets"
-import { MenuIcon, UserIcon } from "lucide-react"
+import { MenuIcon, UserIcon, XIcon } from "lucide-react"
 
 const Sidebar = () => {
 
@@ -25,11 +25,15 @@ const Sidebar = () => {
                 <div className="flex items-center justify-between">
                     <div className="flex items-center gap-3">
                         <UserIcon className="text-white size-7" />
+                        <div>
+                            <p className="font-semibold text-[13px] text-white tracking-wide">Employee MS</p>
+                            <p className="text-[11px] text-slate-500 font-medium">Management System</p>
+                        </div>
                     </div>
-                    <div>
-                        <p className="font-semibold text-[13px] text-white tracking-wide">Employee MS</p>
-                        <p className="text-[11px] text-slate-500 font-medium">Management System</p>
-                    </div>
+                    {/* Close button on mobile  */}
+                    <button onClick={() => setMobileOpen(false)} className="lg:hidden text-slate-400 hover-text-white p-1">
+                        <XIcon size={20} />
+                    </button>
                 </div>
             </div>
 
