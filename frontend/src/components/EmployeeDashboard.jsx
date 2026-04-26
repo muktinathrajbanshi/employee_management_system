ab12a81ce42710258fc97ec776b119bc1f9022f1
unknown <muktinathrajbanshi786@gmail.com>
2026-04-26 11:51:09 +0545

    wow danger mane khatra

--- frontend/src/components/EmployeeDashboard.jsx
+++ frontend/src/components/EmployeeDashboard.jsx
@@ -36,7 +36,17 @@ const EmployeeDashboard = ({data}) => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-5 mb-8">
+        {cards.map((card, index) => {
+            <div key={index} className="card card-hover p-5 sm:p-6 relative overflow-hidden
+            group flex items-center justify-center">
+                <div>
 
+                </div>
+                <card.icon className="size-10 p-2.5 rounded-lg bg-slate-100
+                text-slate-600 group-hover:bg-indigo-50 group-hover:text-indigo-600
+                transition-colors duration-200" />
+            </div>
+        })}
       </div>
     </div>
   )
