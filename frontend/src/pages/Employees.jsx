6daa775eec4b95acfdd88cbddc2c44397ac608ed
unknown <muktinathrajbanshi786@gmail.com>
2026-04-27 21:10:11 +0545

    over nidra lago yar

--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -1,5 +1,20 @@
+import { useCallback, useState } from "react"
+import { dummyEmployeeData } from "../assets/assets"
 
 const Employees = () => {
+
+  const [employees, setEmployees] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchEmployees = useCallback(async () => {
+    setLoading(true)
+    setEmployees(dummyEmployeeData)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
+
+
   return (
     <div>
       Employees
