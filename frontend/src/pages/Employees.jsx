ed0a40c51caf2a422bf3cc011d76279d171f3238
unknown <muktinathrajbanshi786@gmail.com>
2026-04-28 22:24:20 +0545

    la khai ta green signal yar

--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -59,12 +59,15 @@ const Employees = () => {
       {/* employee cards  */}
 
       {loading ? (
-        <div>
+        <div className="flex justify-center p-12">
           <div className="animate-spin h-8 w-8 border-2 border-indigo-600
           border-t-transparent rounded-full" />
         </div>
       ) : (
-        <div></div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
+        xl:grid-cols-4 gap-4 sm:gap-5">
+
+        </div>
       )}
 
 
