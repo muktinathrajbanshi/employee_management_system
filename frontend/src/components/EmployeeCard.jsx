d379af41eec61e8f637fec866e1564a1a759c854
unknown <muktinathrajbanshi786@gmail.com>
2026-04-29 12:55:02 +0545

    do your job bro

--- frontend/src/components/EmployeeCard.jsx
+++ frontend/src/components/EmployeeCard.jsx
@@ -2,7 +2,21 @@
 const EmployeeCard = ({employee, onDelete, onEdit}) => {
   return (
     <div className="group relative card card-holder overflow-hidden">
-      <div></div>
+
+      <div className="relative aspect-4/3 w-full overflow-hidden bg-linear-to-br
+      from-slate-100 to-slate-50">
+
+        <div className="w-full h-full flex items-center justify-center">
+        {/*  circle icon */}
+          <div className="w-20 h-20 rounded-full bg-linear-to from-indigo-100
+          to-slate-100 flex items-center justify-center">
+            <span className="text-2xl font-medium text-indigo-400">
+              {employee.firstName[0]}  {employee.lastName[0]}
+            </span>
+          </div>
+        </div>
+      </div>
+
       <div className="p-5">
         <h3 className="text-slate-900">{employee.firstName} {employee.lastName}</h3>
         <p className="text-xs text-slate-500">{employee.position}</p>
