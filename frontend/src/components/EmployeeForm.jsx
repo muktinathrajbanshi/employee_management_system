de3ac389756e2751d154d6f901331f7e130059da
unknown <muktinathrajbanshi786@gmail.com>
2026-04-29 21:26:18 +0545

    signal todne haha

--- frontend/src/components/EmployeeForm.jsx
+++ frontend/src/components/EmployeeForm.jsx
@@ -13,8 +13,9 @@ const EmployeeForm = ({initialData, onSuccess, onCancel}) => {
     <form onSubmit={handleSubmit} className="space-y-6 max-w-3xl animate-fade-in">
       
     {/* Personal Inormation  */}
-    <div>
-        <h3 className="font-medium mb-6">Personal Information</h3>
+    <div className="card p-5 sm:p-6">
+        <h3 className="font-medium mb-6 pb-4 border-b
+        border-slate-100">Personal Information</h3>
     </div>
 
     </form>
