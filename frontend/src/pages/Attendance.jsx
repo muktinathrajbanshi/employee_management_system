e04cb5f0adaf6facef006a955e2811b97fb9f765
unknown <muktinathrajbanshi786@gmail.com>
2026-04-30 20:13:27 +0545

    hait over nidra lago my lord

--- frontend/src/pages/Attendance.jsx
+++ frontend/src/pages/Attendance.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 
 
 const Attendance = () => {
+  const [history, setHistory] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [isDeleted, setIsDelete] = useState(false)
+
+
+
   return (
     <div>
       Attendance
