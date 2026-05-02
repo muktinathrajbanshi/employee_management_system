cca75f6fe61cbefa90f8ead581f6725f5a3d93d7
unknown <muktinathrajbanshi786@gmail.com>
2026-05-02 13:32:40 +0545

    chhito sakaunu paro yar

--- frontend/src/pages/Leave.jsx
+++ frontend/src/pages/Leave.jsx
@@ -1,5 +1,29 @@
+import { useCallback, useEffect, useState } from "react"
+import { dummyLeaveData } from "../assets/assets"
+import Loading from "../components/Loading"
 
 const Leave = () => {
+
+  const [leaves, setLeaves] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [showModal, setShowModal] = useState(false)
+  const [isDeleted, setIsDeleted] = useState(false)
+  const isAdmin = false;
+
+  const fetchLeaves = useCallback(() => {
+    setLeaves(dummyLeaveData)
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, [])
+
+  useEffect(() => {
+    fetchLeaves()
+  }, [fetchLeaves])
+
+  if(loading) return <Loading />
+
+
   return (
     <div>
       Leave
