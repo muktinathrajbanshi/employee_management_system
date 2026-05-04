6596a865dd49464459fb71c09ce7a0e95ac6c4b9
unknown <muktinathrajbanshi786@gmail.com>
2026-05-04 13:23:31 +0545

    wow kiya baat hain

--- frontend/src/pages/Payslips.jsx
+++ frontend/src/pages/Payslips.jsx
@@ -1,8 +1,42 @@
+import { useCallback, useEffect, useState } from "react"
+import { dummyEmployeeData, dummyPayslipData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const Payslips = () => {
+  const [payslips, setPayslips] = useState([])
+  const [employee, setEmployees] = useState([])
+  const [loading, setLoading] = useState(true);
+  const isAdmin = true;
+
+  const fetchPayslips = useCallback(async () => {
+    setPayslips(dummyPayslipData)
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, [])
+
+  useEffect(() => {
+    fetchPayslips()
+  }, [fetchPayslips])
+
+  useEffect(() => {
+    if(isAdmin) setEmployees(dummyEmployeeData)
+  }, [isAdmin])
+
+
+  if(loading) return <Loading />
+
   return (
-    <div>
-      Payslips
+    <div className="animate-fade-in">
+      <div className="flex flex-col sm:flex-row justify-between items-start
+      sm:items-center gap-4 mb-8">
+        <div>
+          <h1 className="page-title">Payslips</h1>
+          <p className="page-subtitle">{isAdmin ? "Generate and manage employee payslips" : "Your payslip history"}</p>
+        </div>
+        {isAdmin && <p>GENERATE FORM</p>}
+      </div>
+      <p>payslip list</p>
     </div>
   )
 }
