a59b0ba8a3a9e434967f05d543d738cf272fcffb
unknown <muktinathrajbanshi786@gmail.com>
2026-05-05 07:49:08 +0545

    are wah kiya bath hain

--- frontend/src/components/payslip/GeneratePayslipForm.jsx
+++ frontend/src/components/payslip/GeneratePayslipForm.jsx
@@ -1,4 +1,4 @@
-import { Plus } from "lucide-react"
+import { Plus, X } from "lucide-react"
 import { useState } from "react"
 
 const GeneratePayslipForm = ({employees, onSuccess}) => {
@@ -20,8 +20,18 @@ const GeneratePayslipForm = ({employees, onSuccess}) => {
 
 
   return (
-    <div>
-      
+    <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center
+    justify-center z-50 p-4">
+      <div className="card max-w-lg w-full p-6 animate-slide-up">
+        <div className="flex justify-between items-center mb-6">
+            <h3 className="text-lg font-bold text-slate-900">Generate Monthly Payslip</h3>
+            <button 
+            onClick={() => setIsOpen(false)}
+            className="text-slate-400 hover:text-slate-600 p-1">
+                <X size={20} />
+            </button>
+        </div>
+      </div>
     </div>
   )
 }
