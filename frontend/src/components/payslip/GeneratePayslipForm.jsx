5e95da822cf67a212cf294c11799290f9d13b8d6
unknown <muktinathrajbanshi786@gmail.com>
2026-05-05 20:20:36 +0545

    are wah bade bhai kiya bath hain

--- frontend/src/components/payslip/GeneratePayslipForm.jsx
+++ frontend/src/components/payslip/GeneratePayslipForm.jsx
@@ -66,6 +66,13 @@ const GeneratePayslipForm = ({employees, onSuccess}) => {
             </div>
 
             {/* Basic Salary  */}
+             <div>
+                <label className="block text-sm font-medium
+                text-slate-700 mb-2">Basic Salary</label>
+                <input type="number" name="basicSalary" required 
+                placeholder="5000"/>
+            </div>
+
 
             {/* Allowances & Deductions  */}
 
