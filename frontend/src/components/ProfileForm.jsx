1558c2362939dca3dd3859bef845406f21af69ec
unknown <muktinathrajbanshi786@gmail.com>
2026-05-06 12:10:18 +0545

    are wah kiya bath hain

--- frontend/src/components/ProfileForm.jsx
+++ frontend/src/components/ProfileForm.jsx
@@ -1,3 +1,4 @@
+import { User } from "lucide-react";
 import { useState } from "react"
 
 const ProfileForm = ({initialData, onSuccess}) => {
@@ -14,7 +15,7 @@ const ProfileForm = ({initialData, onSuccess}) => {
     <form onSubmit={handleSubmit()} className="card p-5 sm:p-6 mb-6">
         <h2 className="text-base font-medium text-slate-900 mb-6 pb-4
         border-b border-slate-100 flex items-center gap-2">
-            Public Profile
+            <User className="w-5 h-5 text-slate-400" /> Public Profile
         </h2>
     </form>
   )
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { dummyProfileData } from "../assets/assets"
 import Loading from "../components/Loading"
 import { Lock } from "lucide-react"
+import ProfileForm from "../components/ProfileForm"
 
 const Settings = () => {
 
@@ -29,7 +30,7 @@ const Settings = () => {
         <p className="page-subtitle ">Manage your account and preferences</p>
       </div>
 
-      {profile && <p>profile form</p>}
+      {profile && <ProfileForm initialData = {profile} onSuccess={fetchProfile} />}
 
        {/* Change Password trigger  */}
         <div className="card max-w-md p-6 flex items-center justify-between">
