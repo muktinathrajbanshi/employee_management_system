b903a16f57a039c9437f26ec447bf2e9ed7bc192
unknown <muktinathrajbanshi786@gmail.com>
2026-05-06 14:07:25 +0545

    hait k saro alsi lageko

---
+++ frontend/src/components/ChangePasswordModal.jsx
@@ -0,0 +1,33 @@
+import { LockIcon } from "lucide-react"
+import { useState } from "react"
+
+const ChangePasswordModal = ({open, onClose}) => {
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState({type: "", text: ""})
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
+    if(!open) return null;
+
+  return (
+    <div onClick={onClose} 
+    className="fixed inset-0 z-50 flex items-center justify-center p-4">
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-sm"/>
+
+        <div 
+        onClick={(e) => e.stopPropagation()}
+        className="relative bg-white rounded-2xl shadow-2xl w-full max-w-md
+        animate-fade-in">
+            <div>
+                <h2 className="text-lg font-medium text-slate-900 flex items-center gap-2">
+                <LockIcon className="w-5 h-5 text-slate-400" />Chance Password
+                </h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChangePasswordModal
