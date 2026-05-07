60d396df07cbd3a5ad4ba884562ad7f88b0af642
unknown <muktinathrajbanshi786@gmail.com>
2026-05-07 11:32:31 +0545

    aja lai yati yar

---
+++ server/models/Employee.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true}
+
+}, {timestamps: true})
+
+const Employee = mongoose.models.Employee || mongoose.model("Employee", employeeSchema)
+
+export default Employee;
