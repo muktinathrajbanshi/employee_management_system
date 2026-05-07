323ec7156fde8898ccea7b26df1bab3a5efc52fe
unknown <muktinathrajbanshi786@gmail.com>
2026-05-07 11:15:03 +0545

    basic express app created

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import multer from "multer";
+import connectDB from "./config/db.js";
 
 const app = express()
 const PORT = process.env.PORT || 4000;
@@ -10,3 +11,10 @@ const PORT = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 app.use(multer().none())
+
+// Routes
+app.get("/", (req, res) => res.send("Server is running"))
+
+await connectDB()
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)
+)
