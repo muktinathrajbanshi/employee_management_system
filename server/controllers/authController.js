8116537b5406109703aedc12df7a3319af9c79ce
unknown <muktinathrajbanshi786@gmail.com>
2026-05-09 09:43:32 +0545

    let's get started muktinath

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
-
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 // Login for employee and admin
 // POST /api/auth/login
@@ -22,7 +24,26 @@ export const login = async (req, res) => {
         if(role_type === "employee" && user.role !== "EMPLOYEE") {
             return res.status(401).json({ error: "Not authorized as employee" })
         }
+
+        const isValid = await bcrypt.compare(password, user.password)
+        if(!isValid) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const payload = {
+            userId: user._id.toString(),
+            role: user.role,
+            email: user.email,
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, 
+            {expiresIn: "7d"});
+
+            return res.json({ user: payload, token });
+
     } catch (error) {
+        console.error("Login error:", error);
+        return res.status(500).json({ error: "Login failed" });
         
     }
 }
