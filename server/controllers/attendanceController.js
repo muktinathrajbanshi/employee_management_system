4ab2d829f30a9f0d0ba0e6b26267e1d4f6af5b3f
unknown <muktinathrajbanshi786@gmail.com>
2026-05-09 14:35:13 +0545

    amma i am fire right now meu

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -1,4 +1,4 @@
-import Employee from "../models/Employee";
+import Attendance from "../models/Attendance";
 
 
 // Clock in/out for employee
@@ -6,7 +6,36 @@ import Employee from "../models/Employee";
 export const clockInOut = async (req, res) => {
     try {
         const session = req.session;
-        const employee = await Employee
+        const employee = await Employee.findOne({ userId: session.userId })
+        if (!employee) return res.status(404).json({ error: "Employee not found" });
+        
+        if (employee.isDeleted) return res.status(403).json({
+            error: "Your account is deactivated. You cannot clock in/out.",
+        });
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const existing = await Attendance.findOne({
+            employeeId: employee._id,
+            date: today,
+        })
+
+        const now = new Date();
+
+        if(!existing) {
+            const isLate = now.getHours() >= 9 && now.getMinutes() > 0;
+            const attendance = await Attendance.create({
+                employeeId: employee._id,
+                date: today,
+                checkIn: now,
+                status: isLate ? "LATE" : "PRESENT"
+            })
+            return res.json({ success: true, type: "CHECK_IN", data: attendance });
+        } else if(!existing.checkOut){
+            const checkInTime = new Date(existing.checkIn).getTime()
+        }
+
     } catch (error) {
         
     }
