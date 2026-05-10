f6094839dd29dda38a0d2d580709f74f2349428a
unknown <muktinathrajbanshi786@gmail.com>
2026-05-10 15:24:17 +0545

    hait yar kati chhito samay biteko

--- server/controllers/payslipController.js
+++ server/controllers/payslipController.js
@@ -1,8 +1,31 @@
+import Payslip from "../models/Payslip.js";
 
 // Create payslip
 // POST /api/payslips
 export const createPayslip = async (req, res) => {
-    
+    try {
+        const { employeeId, month, year, basicSalary, allowances, deductions } = req.body;
+
+        if(!employeeId || !month || !year || !basicSalary) {
+            return res.status(400).json({ error: "Missing fields" });
+        }
+
+        const netSalary = Number(basicSalary) + Number(allowances || 0) - Number(deductions || 0);
+
+        const payslip = await Payslip.create({
+            employeeId,
+            month: Number(month),
+            year: Number(year),
+            basicSalary: Number(basicSalary),
+            allowances: Number(allowances || 0),
+            deductions: Number(deductions || 0),
+            netSalary,
+        })
+
+        return res.json({ success: true, data: payslip })
+    } catch (error) {
+        return res.status(500).json({ error: "Failed" });
+    }
 }
 
 // Get payslip
