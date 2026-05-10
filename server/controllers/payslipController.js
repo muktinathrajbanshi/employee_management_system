0315d6772909ba3ccea0ffc3253332a1c0b1f23d
unknown <muktinathrajbanshi786@gmail.com>
2026-05-10 13:25:59 +0545

    hait makasam deusai nidra lago yar

---
+++ server/controllers/payslipController.js
@@ -0,0 +1,18 @@
+
+// Create payslip
+// POST /api/payslips
+export const createPayslip = async (req, res) => {
+    
+}
+
+// Get payslip
+// GET /api/payslips
+export const getPayslips = async (req, res) => {
+    
+}
+
+// Get payslip by ID
+// GET /api/payslips/:id
+export const getPayslipById = async (req, res) => {
+    
+}
