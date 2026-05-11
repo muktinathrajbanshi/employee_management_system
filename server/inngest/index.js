78be3507217e284817fd6073915875fc48a3a787
unknown <muktinathrajbanshi786@gmail.com>
2026-05-11 10:02:41 +0545

    hait yar it's totally brilliant

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import Attendance from "../models/Attendance.js";
 import Employee from "../models/Employee.js";
+import LeaveApplication from "../models/LeaveApplication.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "mtech-ems" });
@@ -25,9 +26,38 @@ const autoCheckOut = inngest.createFunction(
         // Send reminder email
 
         // After 10 hours, mark attendance as checked out with status "LATE".
+        await step.sleepUntil("wait-for-the-1-hour", new Date(new Date().getTime() + 1 * 60 * 60 * 1000))
+
+        attendance = await Attendance.findById(attendanceId)
+        if(!attendance?.checkOut) {
+            attendance.checkOut = new Date(attendance.checkIn).getTime() + 4 * 60 * 60 * 1000;
+            attendance.workingHours = 4;
+            attendance.dayType = "Half Day";
+            attendance.status = "LATE";
+            await attendance.save();
+        }
+
     }
   },
 );
 
+// Send Email to admin, If admin doesn't take action on leave application within 24 hours
+const leaveApplicationReminder = inngest.createFunction(
+  { id: "leave-application-reminder"},
+  { event: "leave/pending" },
+  
+  async ({ event, step }) => {
+    const { leaveApplicationId } = event.data;
+
+    // wait for 24 hours
+    await step.sleepUntil("wait-for-the-24-hours", new Date(new Date().getTime() + 24 * 60 * 60 * 1000))
+
+    const leaveApplication = await LeaveApplication
+  }
+
+
+);
+
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [autoCheckOut];
